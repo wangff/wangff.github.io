73c66d46deadc5cd0bb9532c3eaa28366f30b027
Travis CI User <travis@example.org>
2019-05-01 06:50:52 +0000

    Site updated: 2019-05-01 06:50:52

---
+++ js/transition.js
@@ -0,0 +1,1 @@
+$(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){0<$(".sidebar-toc").length&&setTimeout(function(){$("#toggle-sidebar").click()},200)}}),$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
